a9da5dc99b4225ef450c0d6ec07137d80361bc8d
James Attia <145512838+Jatti045@users.noreply.github.com>
2024-10-02 17:14:06 -0400

    Client side (#6)
    * Fixed link around signout button to target entire button not just text signout and removed useNavigate Hook

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,12 +8,12 @@ const Header = () => (
         <LucideBookOpen className="h-8 w-8 text-teal-900" />
         <h1 className="text-2xl font-bold text-teal-900">AccessBridge</h1>
       </div>
-      <button className="flex items-center space-x-2 bg-teal-600 text-white px-4 py-2 rounded-lg hover:bg-teal-700 transition duration-300 ease-in-out">
-        <LogOut size={18} />
-        <Link to="/">
-          <span onClick={() => Navigate("/")}>Sign Out</span>
-        </Link>
-      </button>
+      <Link to="/">
+        <button className="flex items-center space-x-2 bg-teal-600 text-white px-4 py-2 rounded-lg hover:bg-teal-700 transition duration-300 ease-in-out">
+          <LogOut size={18} />
+          <span>Sign Out</span>
+        </button>
+      </Link>
     </div>
   </header>
 );
